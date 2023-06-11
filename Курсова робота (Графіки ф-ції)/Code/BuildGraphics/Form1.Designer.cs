8f11229a9a7c0d372731bc82fdb85b0546b22f7e
V S <87422329+ka0000ce@users.noreply.github.com>
2023-06-11 14:28:20 +0300

    Add files via upload

---
+++ Курсова робота (Графіки ф-ції)/Code/BuildGraphics/Form1.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace BuildGraphics
+{
+    partial class Form1
+    {
+        /// <summary>
+      
+  
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudMin;
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.NumericUpDown nudStep;
+        private System.Windows.Forms.NumericUpDown nudQ;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.RadioButton rbShX;
+        private System.Windows.Forms.RadioButton rbSqrX;
+        private System.Windows.Forms.RadioButton rbExpX;
+    }
+}
+
